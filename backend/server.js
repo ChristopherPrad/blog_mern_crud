8977fdc31a02ca72f756f7517d4e9b2eaf2e41e9
Chris <christopher.pradines@gmail.com>
2019-09-05 09:47:36 +0200

    server

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//MIDDLEWARE SERVER PORT 5000
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+// CONECTION BDD
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+// START SERVER
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
